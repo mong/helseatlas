245d63553316cacec3085a79b1c124623e0d4aa5
Uglemat <mattias.ugelvik@helse-nord.no>
2026-03-02 14:53:42 +0100

    Og nå krasjer det?

--- src/lib/authorization.ts
+++ src/lib/authorization.ts
@@ -1,29 +1,30 @@
 import "server-only";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 
 export async function loginCredentials() {
   if (process.env.NODE_ENV === "development") {
     return { userName: "Example user", email: "example@example.test" };
   }
+  return false;
 
-  const headerList = await headers();
-  const email = headerList.get("x-ms-client-principal-name");
-  const authInfo = headerList.get("x-ms-client-principal");
+  // const headerList = await headers();
+  // const email = headerList.get("x-ms-client-principal-name");
+  // const authInfo = headerList.get("x-ms-client-principal");
 
-  if (!(email && authInfo)) {
-    return false;
-  }
+  // if (!(email && authInfo)) {
+  //   return false;
+  // }
 
-  let parsed;
-  try {
-    parsed = JSON.parse(Buffer.from(authInfo, "base64").toString());
-  } catch (_) {
-    return false;
-  }
+  // let parsed;
+  // try {
+  //   parsed = JSON.parse(Buffer.from(authInfo, "base64").toString());
+  // } catch (_) {
+  //   return false;
+  // }
 
-  const userName: string = (parsed.claims as Array<any>).find(
-    (claim) => claim.typ === "name",
-  ).val;
+  // const userName: string = (parsed.claims as Array<any>).find(
+  //   (claim) => claim.typ === "name",
+  // ).val;
 
-  return { userName, email };
+  // return { userName, email };
 }
