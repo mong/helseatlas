b55276f1d2230c0240c3d393a59de765609f7fe0
Uglemat <mattias.ugelvik@helse-nord.no>
2026-09-09 10:30:52 +0200

    Fjerner ubrukte devDependencies (husky, eslint-config-next og eslint-config-prettier)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,3 @@
-// import path from "node:path";
-// import { fileURLToPath } from "node:url";
-// import js from "@eslint/js";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//     baseDirectory: __dirname,
-//     recommendedConfig: js.configs.recommended,
-//     allConfig: js.configs.all
-// });
-// export default [...compat.extends("next/core-web-vitals", "prettier")];
-
-// @ts-check
-
 import eslint from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
